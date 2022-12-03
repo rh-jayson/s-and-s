fabb0f946d5235220ab48726280383ce0aad7d4c
Jayson Jorgensen <jayson.jorgensen@readinghorizons.com>
2022-12-03 12:21:08 -0700

    doing some wiring up of routing and whatnot

---
+++ src/components/directors.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+export const Directors: FC = () => {
+  return (
+    <>
+      <h1>Directors</h1>
+      <hr />
+      <h2>Director stuff will go here.</h2>
+    </>
+  );
+};
---
+++ src/components/films.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+export const Films: FC = () => {
+  return (
+    <>
+      <h1>Films</h1>
+      <hr />
+      <h2>Film stuff will go here.</h2>
+    </>
+  );
+};
